4bb54d5ed21e56e1014d6139020f85b42fe3e64b
Merygk <mgok85633@gmail.com>
2025-06-01 23:36:04 +0300

    Render için doğru port ayarı yapıldı

--- app.py
+++ app.py
@@ -100,9 +100,14 @@ def modern_sanat_muzesi():
 def hakkimizda():
     return render_template("hakkimizda.html")
 
+import os  # bu en üste alınmalı
+
 # Veritabanını oluştur ve uygulamayı başlat
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    # Render'ın verdiği PORT üzerinden yayına aç
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     
